f330e3dfb41affb194d299035d8090c122e3837d
Joachim Ekström <joachim.ekstroem@gmail.com>
2020-02-25 12:12:06 +0100

    Final project

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
 const url = require('url')
-const app = require('express')()
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 server.listen(8080, () => console.log('Server is up'))
+
 
 const valdata = require('./valdata.json')
 let nuvaranadeVallokal
